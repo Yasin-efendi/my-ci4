c8e4bf7bebdd5bbb3a6eeb7ae96dfbc9e5a1d359
Yasin-efendi <muhyasinefendi@gmail.com>
2025-12-06 16:33:11 +0700

    Menambahkan database migration post

---
+++ app/Database/Migrations/2025-12-06-092315_CreatePostsTable.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreatePostsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true
+            ],
+            'user_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ],
+            'category_id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+            ],
+            'title' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+            ],
+            'slug' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'unique' => true,
+            ],
+            'content' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'image' => [
+                'type' => 'VARCHAR',
+                'constraint' => 255,
+                'null' => true,
+            ],
+            'status' => [
+                'type' => 'ENUM',
+                'constraint' => ['draft', 'published'],
+                'default' => 'draft',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->addForeignKey('user_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('category_id', 'categories', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('posts');            
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('posts');
+    }
+}
